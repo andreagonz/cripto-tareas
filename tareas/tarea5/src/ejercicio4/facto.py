d79b7db4c0457f65552c34c5c813f6cdec9b3fd7
andreagonz <andreagonz@ciencias.unam.mx>
2017-05-19 20:48:51 -0500

    Cosas

--- tareas/tarea5/src/ejercicio4/facto.py
+++ tareas/tarea5/src/ejercicio4/facto.py
@@ -7,7 +7,8 @@ p_mas_q = n - phi + 1
 r1 = (p_mas_q + (math.sqrt((p_mas_q ** 2) - 4 * n))) / 2
 r2 = (p_mas_q - (math.sqrt((p_mas_q ** 2) - 4 * n))) / 2
 
-print(r1)
-print(r2)
-
-print(r1 * r2)
+print("N = " + str(n))
+print("phi(N) = " + str(phi))
+print("r1 = " + str(r1))
+print("r2 = " + str(r2))
+print("r1 * r2 = " + str(int(r1 * r2)))
